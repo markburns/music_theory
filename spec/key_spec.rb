769526dfba094ee989e1b558cd6d5cfd36376fb1
Mark Burns <markthedeveloper@gmail.com>
2013-01-12 13:11:53 +0000

    added a few more examples

--- spec/key_spec.rb
+++ spec/key_spec.rb
@@ -6,9 +6,13 @@ describe Key do
 
   specify do
     notes = key.notes_by_name
-    notes["A4"].midi_number.should == 69
-    notes["A0"].midi_number.should == 21
-    notes["A0"].midi_number.should == 21
+    notes["A4"]  .midi_number.should == 69
+    notes["A0"]  .midi_number.should == 21
+    notes["C0"]  .midi_number.should == 12
+    notes["Bb0"] .midi_number.should == 22
+    notes["A#0"] .midi_number.should == 22
+    notes["B0"]  .midi_number.should == 23
+    notes["C1"]  .midi_number.should == 24
   end
 
   specify do
