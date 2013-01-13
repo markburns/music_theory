97b730ebc75a13214f333f9cb885d090f1154326
Mark Burns <markthedeveloper@gmail.com>
2013-01-13 05:04:09 +0000

    code organisation

--- lib/all.rb
+++ lib/all.rb
@@ -3,15 +3,16 @@ require 'ostruct'
 require 'active_support/core_ext/module/delegation'
 
 require './lib/scale'
-require './lib/twelve_note_scale'
-require './lib/seven_note_scale'
+require './lib/scales/twelve_note_scale'
+require './lib/scales/seven_note_scale'
 
-require './lib/note_diff'
-require './lib/just_intonation'
-require './lib/even_tuning'
+require './lib/tunings/just_intonation'
+require './lib/tunings/even_tuning'
+
 require './lib/key'
 require './lib/key_printer'
-require './lib/scale'
+
 require './lib/note'
+require './lib/note_diff'
 
 
